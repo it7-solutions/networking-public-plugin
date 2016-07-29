618727b93f8998a714a2a462b410a42e047601af
anekhero <buzzer.ua@gmail.com>
2016-07-29 17:53:19 +0300

    fix Observable error

--- app/services/participants.service.ts
+++ app/services/participants.service.ts
@@ -1,28 +1,26 @@
 import {Injectable} from "@angular/core";
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Observable} from 'rxjs/Observable';
-import {Observer} from 'rxjs/Observer';
 
 import {Participant} from "../models/participant";
 
 @Injectable()
 export class ParticipantsService {
     private participants: Participant[];
+    private _onUpdate: BehaviorSubject<Participant[]>;
     public onUpdate: Observable<Participant[]>;
-    private updateObserver: Observer<Participant[]>;
 
     constructor() {
+        console.log('ParticipantsServiceConstructor');
         this.participants = [];
-        this.onUpdate = new Observable<Participant[]>(this.setObserver);
-    }
-
-    setObserver(observer: Observer<Participant[]>){
-        this.updateObserver = observer;
+        this._onUpdate = new BehaviorSubject([]);
+        this.onUpdate = this._onUpdate.asObservable();
     }
 
     setParticipants(participants:Participant[]){
         this.participants = participants;
-        console.log('setParticipants',this.participants.length);
-        this.updateObserver.next(this.participants);
+        console.log('setParticipants !!!',this.participants.length);
+        //this._onUpdate.next(this.participants);
     }
 
     getParticipants(): Participant[]{
