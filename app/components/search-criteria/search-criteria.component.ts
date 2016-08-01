2f56455bb6726ce076b4968a8b9f75ed2256bafd
vherever <vherever@gmail.com>
2016-08-01 18:44:18 +0300

    generating filters from incoming options

--- app/components/search-criteria/search-criteria.component.ts
+++ app/components/search-criteria/search-criteria.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, OnDestroy, Output } from '@angular/core';
 
 import { PluginConfig } from '../../services/plugin.config';
+import { Filter } from "../../models/filter";
 import { KeywordsComponent } from './keywords.component';
 import { FilterComponent } from './filter.component';
 import { MultiFilterComponent } from './multi-filter.component';
@@ -11,8 +12,11 @@ import { MultiFilterComponent } from './multi-filter.component';
     directives: [KeywordsComponent, FilterComponent, MultiFilterComponent],
 })
 export class SearchCriteriaComponent {
+    filters: Filter[];
 
     constructor(private config: PluginConfig) {
+      this.filters = config.filters;
+      console.log(config.filters);
     }
 
 }
